d6ff876b00a324b3de71d64946578490d240f672
amanmehra262001 <amanmehra262001@gmail.com>
2025-05-22 22:57:45 +0530

    update: podcast image and file uploader and deleter

--- podcasts/urls.py
+++ podcasts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PodcastTagListCreateView, PodcastTagDetailView, PodcastListCreateAPIView, PodcastDetailAPIView, PublicPublishedPodcastListAPIView
+from .views import PodcastTagListCreateView, PodcastTagDetailView, PodcastListCreateAPIView, PodcastDetailAPIView, PublicPublishedPodcastListAPIView, S3PodcastsFileManager, S3PodcastsImageManager
 
 urlpatterns = [
     path('tags/', PodcastTagListCreateView.as_view(), name='podcast-tag-list-create'),
@@ -8,4 +8,7 @@ urlpatterns = [
     path('podcasts/', PodcastListCreateAPIView.as_view(), name='podcast-list-create'),
     path('podcasts/<int:pk>/', PodcastDetailAPIView.as_view(), name='podcast-detail'),
     path('podcasts/public/', PublicPublishedPodcastListAPIView.as_view(), name='podcast-public'),
+
+    path('s3/', S3PodcastsFileManager.as_view(), name='podcast-s3-manager'),
+    path('s3-image/', S3PodcastsImageManager.as_view(), name='podcast-s3-image-manager'),
 ]
