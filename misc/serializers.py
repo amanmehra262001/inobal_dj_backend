3c00a43a7ecf7e609bdc6907294c88c9d59d852f
amanmehra262001 <amanmehra262001@gmail.com>
2025-06-27 23:55:46 +0530

    udpate: work_mode_display added as field in careers

--- misc/serializers.py
+++ misc/serializers.py
@@ -2,7 +2,12 @@ from rest_framework import serializers
 from .models import Career
 
 class CareerSerializer(serializers.ModelSerializer):
+    work_mode_display = serializers.SerializerMethodField()
+
     class Meta:
         model = Career
         fields = '__all__'
         read_only_fields = ['created_at', 'updated_at']
+
+    def get_work_mode_display(self, obj):
+        return obj.get_work_mode_display()
